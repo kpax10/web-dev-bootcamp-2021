b9e2302c7eca88facc8c1e0880fb9be4b1945016
Kyle <notxap55@yahoo.com>
2021-11-18 22:53:38 -0800

    Starting conditionals

---
+++ javascript-exercises/conditionals/script.js
@@ -0,0 +1,63 @@
+// let random = Math.random()
+
+// if (random < 0.5) {
+//     console.log("YOUR NUMBER IS LESS THAN 0.5!")
+// } else {
+//     console.log("YOUR NUMBER IS GREATER THAN OR EQUAL TO 0.5!")
+// }
+// console.log(random);
+
+
+// let num = 8
+
+// if (num % 2 === 0) {
+//     console.log("even")
+// }
+
+
+// const dayOfWeek = prompt('ENTER A DAY').toLowerCase();
+
+// if (dayOfWeek === 'monday') {
+//     console.log("UGHHH I HATE MONDAYS")
+// } else if (dayOfWeek === 'saturday') {
+//     console.log("YAY I LOVE SATURDAYS")
+// } else if (dayOfWeek === 'friday') {
+//     console.log("FRIDAYS ARE DECENT, ESPECIALLY AFTER WORK!")
+// } else {
+//     console.log("MEH")
+// }
+
+
+// 0-5 = FREE
+// 5-10 = CHILD $10
+// 10-65 = ADULT $20
+// 65+ = SENIOR $10
+
+// const age = 89;
+
+// if (age < 5) {
+//     console.log("You are a baby. You get in for free!")
+// } else if (age < 10) {
+//     console.log("You are a child. You get in for $10!")
+// } else if (age < 65) {
+//     console.log("You are an adult. You get in for $20!")
+// } else {
+//     console.log("You are a senior. You get in for $10!")
+// }
+
+// let phrase = "slow";
+
+// if (phrase === "stop") {
+//     console.log("red")
+// } if (phrase === 'slow') {
+//     console.log('yellow')
+// } else if (phrase === 'go') {
+//     console.log('green')
+// } else {
+//     console.log('purple')
+// }
+
+const password = prompt("please enter a new password");
+
+//Password must be 6+ characters
+//Password cannot include space
