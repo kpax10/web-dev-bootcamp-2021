29e5b819e80cbe92bd76ac278c43336ea520196c
Kyle <notxap55@yahoo.com>
2021-11-19 15:49:37 -0800

    Finished Javascript operators

--- javascript-exercises/conditionals/script.js
+++ javascript-exercises/conditionals/script.js
@@ -60,4 +60,13 @@
 const password = prompt("please enter a new password");
 
 //Password must be 6+ characters
-//Password cannot include space
+if (password.length >= 6) {
+    //Password cannot include space
+    if (password.indexOf(" ") === -1) {
+        console.log("Valid Password")
+    } else {
+        console.log("Password cannot contain spaces")
+    }
+} else {
+    console.log("Password too short!  Must be 6+ characters.")
+}
