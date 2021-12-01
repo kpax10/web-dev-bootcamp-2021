4958aad6efff0e36575c1145b00ddeae0e6d3ffe
Kyle <notxap55@yahoo.com>
2021-11-30 21:54:03 -0800

    ForOf and iterating objects

---
+++ javascript-exercises/iterating-objects/script.js
@@ -0,0 +1,26 @@
+const testScores = {
+    keenan: 80,
+    damon: 67,
+    kim: 89,
+    shawn: 91,
+    marlon: 72,
+    dwayne: 77,
+    nadia: 83,
+    elvira: 97,
+    diedre: 81,
+    vonnie: 60,
+}
+
+// for (let person in testScores) {
+//     console.log(`${person} scored ${testScores[person]}`);
+// }
+
+
+//sum up all the values and average them//
+let total = 0;
+let scores = Object.values(testScores);
+
+for (let score of scores) {
+    total += score;
+}
+console.log(total / scores.length)
