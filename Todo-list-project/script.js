c406f52668011e50773713a42c02507cb4554ff3
Kyle <notxap55@yahoo.com>
2021-12-02 18:25:41 -0800

    initial commit

---
+++ Todo-list-project/script.js
@@ -0,0 +1,21 @@
+const initQuestion = prompt("What would you like to do?");
+const toDoList = [];
+
+while (initQuestion === "quit") break; {
+    console.log("OK, YOU QUIT THE APP")
+}
+
+while (initQuestion = "new") {
+    console.log("Enter New Todo")
+}
+
+
+
+
+
+
+
+
+// while promp = new, add object to array
+//while prompt = list, console.log array
+//while prompt = delete, prompt for "enter the index of the item to delete "
