8fb1c80446ec5a31b377c56353ce35c336568c12
Kyle <notxap55@yahoo.com>
2021-12-03 15:50:23 -0800

    delete function started

--- Todo-list-project/script.js
+++ Todo-list-project/script.js
@@ -3,25 +3,31 @@ let toDoList = [];
 
 let initQuestion = prompt('What would you like to do?')
 while (initQuestion !== 'quit') {
-    if (initQuestion === "new") {
+    //add item function
+    if (initQuestion === 'new') {
         toDoList.push(prompt('Enter new todo:'))
         let displayItem = toDoList[toDoList.length - 1];
         console.log(`${displayItem} added to list`)
     }
-    if (initQuestion === "list") {
-        // let arrLocation = toDoList.length - 1;
+    //list function
+    if (initQuestion === 'list') {
         console.log('************')
         for (let items in toDoList) {
             console.log(`${items}: ${toDoList[items]}`)
         }
         console.log('************')
     }
+    //delete function
+    if (initQuestion === 'delete') {
+        console.log('delete')
+    }
+
     initQuestion = prompt('What would you like to do?')
 }
 
 
-if (initQuestion === "quit") {
-    console.log("YOU QUIT")
+if (initQuestion === 'quit') {
+    console.log('You quit')
 }
 
 
