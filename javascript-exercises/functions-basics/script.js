ab02446182e027d2512d6ab590d08a2ddafe6365
Kyle <notxap55@yahoo.com>
2021-12-13 15:44:06 -0800

    functions basics started

---
+++ javascript-exercises/functions-basics/script.js
@@ -0,0 +1,37 @@
+// function singSong() {
+//     console.log('DO');
+//     console.log('RE');
+//     console.log('ME');
+// }
+
+// singSong()
+
+function greet(firstName, lastName) {
+    console.log(`Hey there, ${firstName} ${lastName[0]}.`)
+}
+
+
+
+function repeat(word, numTimes) {
+    let result = '';
+    for (let i = 0; i < numTimes; i++) {
+        result += word;
+    }
+    console.log(result);
+}
+
+
+
+// greet('Elvis');  Elvis will be assigned to firstName
+
+
+//with loops//
+// function rant(message) {
+//     for (let i = 0; i < 3; i++) {
+//         console.log(message.toUpperCase());
+//     }
+    //without loops//
+    // console.log(message.toUpperCase());
+    // console.log(message.toUpperCase());
+    // console.log(message.toUpperCase());
+// }
