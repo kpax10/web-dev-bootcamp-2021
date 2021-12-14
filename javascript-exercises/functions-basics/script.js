846adcd87d3494500f3743ddd05684880746252b
Kyle <notxap55@yahoo.com>
2021-12-13 22:24:36 -0800

    More functions basics

--- javascript-exercises/functions-basics/script.js
+++ javascript-exercises/functions-basics/script.js
@@ -6,19 +6,19 @@
 
 // singSong()
 
-function greet(firstName, lastName) {
-    console.log(`Hey there, ${firstName} ${lastName[0]}.`)
-}
+// function greet(firstName, lastName) {
+//     console.log(`Hey there, ${firstName} ${lastName[0]}.`)
+// }
 
 
 
-function repeat(word, numTimes) {
-    let result = '';
-    for (let i = 0; i < numTimes; i++) {
-        result += word;
-    }
-    console.log(result);
-}
+// function repeat(word, numTimes) {
+//     let result = '';
+//     for (let i = 0; i < numTimes; i++) {
+//         result += word;
+//     }
+//     console.log(result);
+// }
 
 
 
@@ -30,8 +30,65 @@ function repeat(word, numTimes) {
 //     for (let i = 0; i < 3; i++) {
 //         console.log(message.toUpperCase());
 //     }
-    //without loops//
-    // console.log(message.toUpperCase());
-    // console.log(message.toUpperCase());
-    // console.log(message.toUpperCase());
+//without loops//
+// console.log(message.toUpperCase());
+// console.log(message.toUpperCase());
+// console.log(message.toUpperCase());
 // }
+
+
+// function isSnakeEyes(firstRoll, secondRoll) {
+//     if (firstRoll === 1) {
+//         if (secondRoll === 1) {
+//             console.log('Snake Eyes!')
+//         } else {
+//             console.log('Not Snake Eyes!')
+//         }
+//     } else {
+//         console.log('Not Snake Eyes!')
+//     }
+// }
+
+// function isSnakeEyes(firstRoll, secondRoll) {
+//     if (firstRoll == 1 && secondRoll == 1) {
+//         console.log('Snake Eyes!');
+//     } else {
+//         console.log('Not Snake Eyes!');
+//     }
+// }   
+
+
+// function add(x, y) {
+//     if (typeof x !== 'number' || typeof y !== 'number') {
+//         return false;
+//     }
+
+//     return x + y;
+// }
+
+
+// function multiply(firstNum, secondNum) {
+//     return firstNum * secondNum;
+// }
+
+
+// function isShortsWeather(temperature) {
+//     if (temperature >= 75) {
+//         return true;
+//     } return false;
+// }
+
+
+// function lastElement(array) {
+//     if (array.length === 0) {
+//         return null;
+//     }
+//     return array[array.length - 1];
+// }
+
+
+function capitalize(input) {
+    const firstLetter = input.slice(0, 1);
+    const lastLetters = input.slice(1);
+    return firstLetter.toUpperCase() + lastLetters;
+}
