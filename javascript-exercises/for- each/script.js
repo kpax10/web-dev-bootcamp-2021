3c11e7a1c2d76988270c9bd0be6cc7083edb28b2
Kyle <notxap55@yahoo.com>
2021-12-17 11:56:33 -0800

    for each lesson added

--- javascript-exercises/for- each/script.js
+++ javascript-exercises/for- each/script.js
@@ -1,2 +1,38 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
+
+// numbers.forEach(function (el) { //Will print even numbers//
+//     if (el % 2 === 0) {
+//         console.log(el);
+//     }
+// })
+
+// for (let el of numbers) {
+//     console.log(el);
+// }
+
+
+
+const movies = [
+    {
+        title: 'Amadeus',
+        score: 99
+    },
+    {
+        title: 'Stand By Me',
+        score: 85
+    },
+    {
+        title: 'Parasite',
+        score: 95
+    },
+    {
+        title: 'Alien',
+        score: 90
+    }
+]
+//Anonymous function expression 'movie' exists just for this block. 
+//Movie is each element from the array, being passed into this function
+movies.forEach(function (movie) {
+    console.log(`${movie.title} - ${movie.score}/100`)
+})
