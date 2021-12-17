b7d6af9f9085e5a25c471e88433b06588c2d494b
Kyle <notxap55@yahoo.com>
2021-12-16 22:04:37 -0800

    For each lesson started

---
+++ javascript-exercises/try-catch/script.js
@@ -0,0 +1,17 @@
+
+// try {
+//     hello.toUpperCase();
+// } catch {
+//     console.log('ERROR')
+// }
+
+// console.log('AFTER')
+
+
+function yell(msg) {
+    try {
+        console.log(msg.toUpperCase().repeat(3));
+    } catch (e) {
+        console.log('Please use a string')
+    }
+}
