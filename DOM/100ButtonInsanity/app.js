5553f59dbbe725f887c8ea315bcad23b07d75988
Kyle <notxap55@yahoo.com>
2021-12-28 21:59:30 -0800

    100 button insanity completed

--- DOM/100ButtonInsanity/app.js
+++ DOM/100ButtonInsanity/app.js
@@ -1,4 +1,9 @@
+//Creates 100 buttons with text. assigns the buttons to the container div.
+
 const container = document.querySelector('#container');
-const button = document.createElement(button);
-button.innerText = 'Woo'
-document.container.appendChild(button);
+
+for (let i = 0; i < 100; i++) {
+    const button = document.createElement('button');
+    button.innerText = 'Woo!';
+    container.appendChild(button);
+}
