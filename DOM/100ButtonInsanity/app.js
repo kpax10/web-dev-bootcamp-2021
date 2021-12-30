f20d999c14279b2002c76812cd76b3426ead6c99
Kyle <notxap55@yahoo.com>
2021-12-29 21:14:19 -0800

    Pokemon completed

--- DOM/100ButtonInsanity/app.js
+++ DOM/100ButtonInsanity/app.js
@@ -7,3 +7,5 @@ for (let i = 0; i < 100; i++) {
     button.innerText = 'Woo!';
     container.appendChild(button);
 }
+
+//
--- DOM/Pokemon/app.js
+++ DOM/Pokemon/app.js
@@ -1,9 +1,24 @@
 // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
 
+// const container = document.querySelector('.container');
+
+// for (let i = 0; i < 151; i++) {
+//     const newImg = document.createElement('img');
+//     newImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i + 1}.png`;
+//     container.append(newImg, `${i + 1}`);
+// }
+
 const container = document.querySelector('.container');
 
-for (let i = 0; i < 10; i++) {
-    const newImg = document.createElement('img');
-    newImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i + 1}.png`;
-    container.append(newImg, `${i + 1}`);
+for (let i = 0; i < 151; i++) {
+    const div = document.createElement('div');
+    div.classList.add('pokemon')
+    const img = document.createElement('img');
+    img.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i + 1}.png`;
+    const span = document.createElement('span');
+    span.innerText = `${i + 1}`;
+
+    container.append(div);
+    div.append(img);
+    div.append(span);
 }
