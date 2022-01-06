cf4edd77dd85126e283b190369295c5a268015e3
Kyle <notxap55@yahoo.com>
2022-01-06 15:59:49 -0800

    Change and Event inputs started

---
+++ DOM/ChangeAndInputEvents/app.js
@@ -0,0 +1,22 @@
+// const input = document.querySelector('input');
+
+// input.addEventListener('change', function (e) {
+//     console.log('change!');
+// })
+
+// input.addEventListener('input', function (e) {
+//     console.log('input!');
+// })
+
+
+//update h1 with whatever you type as you type it
+const h1 = document.querySelector('h1');
+const input = document.querySelector('input');
+
+
+//target form
+input.addEventListener('input', function (e) {
+    console.log('inputs');
+})
+//add event listener for input then assign variable to the input?
+// add innerText to h1
