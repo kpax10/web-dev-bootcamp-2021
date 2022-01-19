54b608d77d72cb4422921db81a1b8ff967da4e3a
Kyle <notxap55@yahoo.com>
2022-01-18 21:51:50 -0800

    Callback hell added

---
+++ DOM/CallbackHell/app.js
@@ -0,0 +1,46 @@
+// setTimeout(() => {
+//     document.body.style.backgroundColor = 'red';
+//     setTimeout(() => {
+//         document.body.style.backgroundColor = 'orange';
+//         setTimeout(() => {
+//             document.body.style.backgroundColor = 'yellow';
+//             setTimeout(() => {
+//                 document.body.style.backgroundColor = 'green';
+//                 setTimeout(() => {
+//                     document.body.style.backgroundColor = 'blue';
+//                 }, 1000)
+//             }, 1000)
+//         }, 1000)
+//     }, 1000)
+// }, 1000)
+
+
+const delayedColorChange = (newColor, delay, doNext) => {
+    setTimeout(() => {
+        document.body.style.backgroundColor = newColor;
+        doNext();
+    }, delay)
+}
+
+delayedColorChange('red', 3000, () => {
+    delayedColorChange('orange', 1000, () => {
+        delayedColorChange('yellow', 1000, () => {
+            delayedColorChange('green', 1000, () => {
+                delayedColorChange('blue', 1000, () => {
+                })
+            })
+        })
+    })
+});
+
+
+//  VERY COMMON CODE SEGMENT//
+searchMovieAPI('amadeus', () => {
+    saveToMyDB(movies, () => {
+        //if it works, run this:
+    }), () => {
+        //if it doesn't work, run this:
+    }
+}, () => {
+    //if API is down, or request failed:
+})
